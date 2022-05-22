ff83cb142256c124926cb1d9de224dc1e4811d6c
ShamenPY <karolswwacha17@gmail.com>
2022-05-22 21:07:43 +0200

    has been added rect of enemy and main character (you) has been added a moving of you

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import pygame
-import random
+
 width,height = 1920,1160
 
 map_img = pygame.image.load("images\\background\\middle_map.png")
-you = pygame.image.load("images\\characters\\you.png")
-y = pygame.mask.from_surface(you)
-rectyou = you.get_rect()
-enemy = pygame.image.load("images\\characters\\enemy.png")
+youimg = pygame.image.load("images\\characters\\you.png")
+you = pygame.transform.scale(youimg,(100,100))
+enemyimg = pygame.image.load("images\\characters\\enemy.png")
+enemy = pygame.transform.scale(enemyimg,(100,100))
 map = pygame.transform.scale(map_img,(1920,1160))
-recty = pygame.mask.Mask.get_rect(y)
-x = pygame.mask.from_surface(enemy)
-rectenemy = enemy.get_rect()
-
 
 screen = pygame.display.set_mode((width,height))
 
@@ -19,18 +15,10 @@ position_of_mouse = pygame.mouse.get_pos()
 class App():
     def __init__(self):
 
-        self.posX = 500 #VALUE OF X AND Y YOU
-        self.posY = 500
-        self.rectyouX = self.posX + 84
-        self.rectyouY = self.posY + 112.5
-        self.enemyX = 300 #VALUE OF X AND Y ENEMY
-        self.enemyY = 300
-        self.rectenemyX = self.enemyX + 85
-        self.rectenemyY = self.enemyY + 115.5
-
-
-
-
+        self.yourposX = 500 #VALUE OF X AND Y YOU
+        self.yourposY = 500
+        self.enemyposX = 300 #VALUE OF X AND Y ENEMY
+        self.enemyposY = 300
         self.main()
 
     def events_handling(self):
@@ -40,35 +28,40 @@ class App():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    self.posX -= 30
+                    self.yourposX -= 30
                 if event.key == pygame.K_RIGHT:
-                    self.posX += 30
+                    self.yourposX += 30
                 if event.key == pygame.K_UP:
-                    self.posY -= 30
+                    self.yourposY -= 30
                 if event.key == pygame.K_DOWN:
-                    self.posY += 30
+                    self.yourposY += 30
+
+        self.rectenemyX = self.enemyposX + 50
+        self.rectenemyY = self.enemyposY + 50
+        self.rectyouX = self.yourposX + 50
+        self.rectyouY = self.yourposY + 50
+        self.rectofyou = you.get_rect(center=(self.rectyouX, self.rectyouY))
+        self.rectofenemy = enemy.get_rect(center=(self.rectenemyX, self.rectenemyY))
+        self.collide = pygame.Rect.colliderect(self.rectofyou, self.rectofenemy)
+
+        if self.collide:
+            print("jest kolizja :) ")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+    def draw_screen(self):
+        screen.blit(map, (0, 0))
+        screen.blit(you, (self.yourposX, self.yourposY))
+        screen.blit(enemy, (self.enemyposX, self.enemyposY))
 
 
     def main(self):
         running = True
         while True:
 
-            self.rectofyou = you.get_rect(center=(self.rectyouX , self.rectyouY))
-            self.rectofenemy = enemy.get_rect(center=(self.rectenemyX, self.rectenemyY))
-            self.collide = pygame.Rect.colliderect(self.rectofyou, self.rectofenemy)
-
-
-            if self.collide:
-                print("jest kolizja :) ")
-            screen.blit(map,(0,0))
-            screen.blit(you,(self.posX,self.posY))
-            screen.blit(enemy,(self.enemyX,self.enemyY))
+            self.draw_screen()
             self.events_handling()
-            print(self.posX, self.posY)
-            print(self.position_of_mouse)
+
             pygame.display.flip()
 
 App = App()
