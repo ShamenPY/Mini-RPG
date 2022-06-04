36525e86739fb2ce94a9e6806c3230730198894e
systemm4tic <kaczybura.dawid@gmail.com>
2022-06-04 09:25:18 +0200

    Start working on player movements

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ position_of_mouse = pygame.mouse.get_pos()
 class App():
     def __init__(self):
         self.running = True
+        self.movement = [0,0]
+        self.speed = 6
+        
         self.yourposX = 500 #VALUE OF X AND Y YOU
         self.yourposY = 500
         self.enemyposX = 300 #VALUE OF X AND Y ENEMY
@@ -33,13 +36,25 @@ class App():
                     self.running = False
                     return
                 if event.key == pygame.K_LEFT:
-                    self.yourposX -= 30
+                    self.movement[0] -= 1
                 if event.key == pygame.K_RIGHT:
-                    self.yourposX += 30
+                    self.movement[0] += 1
                 if event.key == pygame.K_UP:
-                    self.yourposY -= 30
+                    self.movement[1] -= 1
                 if event.key == pygame.K_DOWN:
-                    self.yourposY += 30
+                    self.movement[1] += 1
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    self.movement[0] += 1
+                if event.key == pygame.K_RIGHT:
+                    self.movement[0] -= 1
+                if event.key == pygame.K_UP:
+                    self.movement[1] += 1
+                if event.key == pygame.K_DOWN:
+                    self.movement[1] -= 1
+
+        self.yourposX += self.movement[0] * self.speed
+        self.yourposY += self.movement[1] * self.speed
 
         self.rectenemyX = self.enemyposX + 50
         self.rectenemyY = self.enemyposY + 50
